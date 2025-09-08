55a29d363093e42f1ef873cd093ad65a62cd0aff
Gustavo Gallegos <gustavogallegosordenes@gmail.com>
2025-09-08 20:18:48 -0300

    se añade padre Usuario

---
+++ src/main/java/com/mycompany/prueba1/Usuario.java
@@ -0,0 +1,17 @@
+package com.mycompany.prueba1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private byte edad;
+    
+}
