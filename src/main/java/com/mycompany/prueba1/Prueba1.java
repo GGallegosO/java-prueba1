cbaec363fa63bc30eaae6c1b03e4d338960ebf85
Gustavo Gallegos <gustavogallegosordenes@gmail.com>
2025-09-08 19:58:27 -0300

    init project

---
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.err.println("Este es un error");
+       
+        
+        String  mensaje = "Este es un mensaje";
+        System.out.println(mensaje); // sout  ctrl + space  = autocompletado
+     
+    }
+}
+
