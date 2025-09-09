1138d781309f11a5337e3d2d8e604eb324292b3d
Gustavo Gallegos <gustavogallegosordenes@gmail.com>
2025-09-08 21:40:10 -0300

    Se añadio usuario y alumno

--- src/main/java/com/mycompany/prueba1/Prueba1.java
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -18,6 +18,22 @@ public class Prueba1 {
         String  mensaje = "Este es un mensaje"; // variable String
         System.out.println(mensaje); // sout  ctrl + space  = autocompletado
      
+        //instanciar e invocar
+    
+        //Usuario user1 = new Usuario();
+        Usuario user1 = new Usuario("Gustavo", "Gallegos",(byte)21,true,"Belga");
+        Usuario user2 = new Usuario("Blinky", "Bluz",(byte)41,true,"Argentino");
+        Usuario user3 = new Usuario("Clyde", "Schweder",(byte)35,true,"Alemán");
+        Usuario user4 = new Usuario("Bloom", "Pou",(byte)21,true,"Chileno");
+    
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
+        user2.setNombre("Tomas");
+        System.out.println(user2.toString());
+        System.out.println(user4.toString());
+        
+        Alumno alumno = new Alumno (2, 3, 1, "Mateo", "Pizarro", (byte)22, true, "Chileno");
+        alumno.saludar();
     }
 }
 
